517ee78bae453027c4f3ed416629181eda6f2436
Diego-1984 <diegomenoruceta@gmail.com>
2023-06-20 18:49:48 +0000

    Solo falata poder editar

--- src/js/views/editarcontacto.js
+++ src/js/views/editarcontacto.js
@@ -1,78 +1,105 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Context } from "../store/appContext";
 import "../../styles/demo.css";
 
 export const EditContact = () => {
-	const navigate = useNavigate()
-	const { store, actions } = useContext(Context);
-	const [data, setData] = useState({
-		full_name: "",
-        email: "",
-        agenda_slug: "Diego3",
-        address:"",
-        phone:""
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    full_name: "",
+    email: "",
+    agenda_slug: "Diego3",
+    address: "",
+    phone: "",
+  });
 
-	})
-	
-	const handleChange =(event)=>{
-		setData({...data,[event.target.name]:event.target.value})
-	}
-	const handleSubmit = (event) => {
-        event.preventDefault()
-        // hacemos un fetch para enviar la información a la API
-        const config = {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {'Content-Type': 'application/json'}
-        }
-        fetch('https://assets.breatheco.de/apis/fake/contact/', config)
-            .then(res => res.json())
-            .catch(error => console.error('Error:', error))
-            .then(response => navigate('/'));
-    }
-	return (
-		<div className="container-fluid bg-black">	
-			
-			<div className="container w-50 vh-100 bg-white">
-							
-				<div className="d-flex justify-content-center p-3"><h1>Editar contacto</h1></div>
-				<form onSubmit={handleSubmit}>
-				<div className="input flex-nowrap mt-3">
-				Nombre completo  				
-  				<input onChange={handleChange} name= "full_name" type="text" className="form-control mt-2" placeholder="Inserta nombre completo" aria-label="Username" aria-describedby="addon-wrapping"/>
-				</div>
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // hacemos un fetch para enviar la información a la API
+    const config = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("https://assets.breatheco.de/apis/fake/contact/", config)
+      .then((res) => res.json())
+      .then((response) => navigate("/"));
+  };
+  return (
+    <div className="container-fluid bg-black">
+      <div className="container w-50 vh-100 bg-white">
+        <div className="d-flex justify-content-center p-3">
+          <h1>Editar contacto</h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input flex-nowrap mt-3">
+            Nombre completo
+            <input
+              onChange={handleChange}
+              name="full_name"
+              type="text"
+              className="form-control mt-2"
+              placeholder="Inserta nombre completo"
+              aria-label="Username"
+              aria-describedby="addon-wrapping"
+            />
+          </div>
 
+          <div className="input flex-nowrap mt-3">
+            E-mail
+            <input
+              onChange={handleChange}
+              name="email"
+              type="text"
+              className="form-control mt-2"
+              placeholder="Username"
+              aria-label="Inserta e-mail"
+              aria-describedby="addon-wrapping"
+            />
+          </div>
 
-				<div className="input flex-nowrap mt-3">
-				E-mail  				
-  				<input onChange={handleChange} name="email" type="text" className="form-control mt-2" placeholder="Username" aria-label="Inserta e-mail" aria-describedby="addon-wrapping"/>
-				</div>
+          <div className="input flex-nowrap mt-3">
+            Teléfono
+            <input
+              onChange={handleChange}
+              name="phone"
+              type="number"
+              className="form-control mt-2"
+              placeholder="Número completo"
+              aria-label="Inserta número completo"
+              aria-describedby="addon-wrapping"
+            />
+          </div>
 
-				<div className="input flex-nowrap mt-3">
-				Teléfono  				
-  				<input onChange={handleChange} name="phone" type="number" className="form-control mt-2" placeholder="Número completo" aria-label="Inserta número completo" aria-describedby="addon-wrapping"/>
-				</div>
+          <div className="input flex-nowrap mt-3">
+            Dirección
+            <input
+              onChange={handleChange}
+              name="address"
+              type="text"
+              className="form-control mt-2"
+              placeholder="Inserta dirección completa"
+              aria-label="Username"
+              aria-describedby="addon-wrapping"
+            />
+          </div>
 
-				<div className="input flex-nowrap mt-3">
-				Dirección  				
-  				<input onChange={handleChange} name="address" type="text" className="form-control mt-2" placeholder="Inserta dirección completa" aria-label="Username" aria-describedby="addon-wrapping"/>
-				</div>
-
-				<div className="d-grid gap-2 mt-3">
-  				<button className="btn btn-primary" type="submit">Guardar</button>  
-				</div>
-				</form>
-				<div className="footer">
-					<Link to="/">
-				<button className="btn btn-link">Volver a la lista de contactos</button>
-					</Link>
-				</div>
-				
-		</div>
-	</div>
-			
-			
-			
-	);
+          <div className="d-grid gap-2 mt-3">
+            <button className="btn btn-primary" type="submit">
+              Guardar
+            </button>
+          </div>
+        </form>
+        <div className="footer">
+          <Link to="/">
+            <button className="btn btn-link">
+              Volver a la lista de contactos
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,89 +1,81 @@
 import React from "react";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
-import { useState, useEffect, useNavigate} from "react";
-
+import { useState, useEffect } from "react";
 
 export const Home = () => {
-	const [contacts, setContacts]=useState([]);
-	useEffect(()=>{ fetch ("https://assets.breatheco.de/apis/fake/contact/agenda/Diego3")
-	.then(response=>response.json())
-	.then(contacts=>{setContacts(contacts)})
-	.then(response=>console.log(response))
-	})
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Diego3")
+      .then((response) => response.json())
+      .then((contacts) => {
+        setContacts(contacts);
+      })
+      .then((response) => console.log(response));
+  });
 
-	const handleDelete = (id) => {
-		const config = {
-			method: 'DELETE',
-			
-		};
+  const handleDelete = (id) => {
+    const config = {
+      method: "DELETE",
+    };
 
-		fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, config)
-			.then(res => res.json())						
-			.catch(error => console.error('Error:', error));
-	};
+    fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, config)
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error));
+  };
 
-	const handleEdit = (id) => {
-			const config = {
-			method: 'PUT',
-			body:{
-        		full_name: [],
-       			email: [],
-        		agenda_slug: "Diego3",
-        		address:[],
-        		phone:[]
-    }
-		};
-		fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, config)
-			.then(res => res.json())
-			.then(data => {
-				console.log(data);
-			})
-			.catch(error => console.error('Error:', error));
-	};
-	
-	
-	return (
-		<>
-			<div className="container-fluid w-50 justify-content-end d-flex bg-white">
-				<Link to="/addcontact">
-					<button className="btn btn-success mt-3 my-3 mx-2 ">Añadir nuevo contacto</button>
-				</Link>
-			</div>
-			
-			<div className="container w-50 vh-100 bg-white">			
-				
-				
-				{contacts.map((contact, index)=>(
-					
-		<div key={index}className="row border mx-2">
-        <div className="col-3 mt-2">
-            <img src="https://media.istockphoto.com/id/1135031219/es/vector/usuario-miembro-de-perfil-de-icono-de-hombre-vector-de-s%C3%ADmbolo-perconal-en-fondo-aislado.jpg?s=170667a&w=0&k=20&c=vacSQk6KSFhZ8YbdZu-i83QMA4LN9MrkRb7tFUGbJcs=" alt="Profile"style={{ width: "125px", height: "125px" }} />
-        </div>
+  return (
+    <>
+      <div className="container-fluid w-50 justify-content-end d-flex bg-white">
+        <Link to="/addcontact">
+          <button className="btn btn-success mt-3 my-3 mx-2 ">
+            Añadir nuevo contacto
+          </button>
+        </Link>
+      </div>
 
-        <div className="col-7">
-            <div className="my-1"><h5>{contact.full_name}</h5></div>
-            <div className="my-1"><i className="fa-solid fa-map-pin"></i>   {contact.address}</div>
-            <div className="my-1"><i className="fa-solid fa-envelope"></i>   {contact.email}</div>
-            <div className="my-1"><i className="fa-solid fa-square-phone-flip"></i>   {contact.phone}</div>
-        </div>
+      <div className="container w-50 vh-100 bg-white">
+        {contacts.map((contact, index) => (
+          <div key={index} className="row border mx-2">
+            <div className="col-3 mt-2">
+              <img
+                src="https://media.istockphoto.com/id/1135031219/es/vector/usuario-miembro-de-perfil-de-icono-de-hombre-vector-de-s%C3%ADmbolo-perconal-en-fondo-aislado.jpg?s=170667a&w=0&k=20&c=vacSQk6KSFhZ8YbdZu-i83QMA4LN9MrkRb7tFUGbJcs="
+                alt="Profile"
+                style={{ width: "125px", height: "125px" }}
+              />
+            </div>
 
-        <div className="col-2 my-3">		
-            <button onClick={() => handleDelete(contact.id)}><i className="fa-solid fa-trash-can"></i></button>
-            <br/>
-            <br/>
-			<Link to="/editcontact">
-            <button><i className="fa-solid fa-pencil"></i></button>
-			</Link>
-        </div>
-      
-    </div>
-				
-				))}
-				
-		
-			</div>						
-		
-		</>
-	);
+            <div className="col-7">
+              <div className="my-1">
+                <h5>{contact.full_name}</h5>
+              </div>
+              <div className="my-1">
+                <i className="fa-solid fa-map-pin"></i> {contact.address}
+              </div>
+              <div className="my-1">
+                <i className="fa-solid fa-envelope"></i> {contact.email}
+              </div>
+              <div className="my-1">
+                <i className="fa-solid fa-square-phone-flip"></i>{" "}
+                {contact.phone}
+              </div>
+            </div>
+
+            <div className="col-2 my-3">
+              <button onClick={() => handleDelete(contact.id)}>
+                <i className="fa-solid fa-trash-can"></i>
+              </button>
+              <br />
+              <br />
+              <Link to="/editcontact">
+                <button>
+                  <i className="fa-solid fa-pencil"></i>
+                </button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
